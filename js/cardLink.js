5a1a8b33c3488466b123cbf30a13dbc035b9d558
Melcocha <melanyvp98@gmail.com>
2023-07-28 23:17:45 -0600

    compatibilidad otros buscadores web

---
+++ js/cardLink.js
@@ -0,0 +1,8 @@
+const cardsProductos = document.querySelectorAll('.galeria__productos_items');
+const verProductos = document.querySelectorAll('.galeria__productos--link');
+
+cardsProductos.forEach((item) => {
+    item.addEventListener('hover', () => {
+        
+    });
+});
