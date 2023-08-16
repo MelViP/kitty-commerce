775c1dc3d68735729c4667621b715a17f46c1037
Mel <melanyvp98@gmail.com>
2023-08-15 19:31:23 -0600

    agregando prod desde json a index

--- js/controllers/products-controller.js
+++ js/controllers/products-controller.js
@@ -20,7 +20,92 @@
 
 import { productServices } from "../../service/product-service.js"
 
-const agregarProd = (imagen, nombre, precio, id) => {
+//agregar prod salud y bienestar
+
+const agregarProdSaludBienestar = (imagen, nombre, precio, id) => {
+    const linea = document.createElement("li");
+    linea.classList.add("galeria__productos_items");
+    const content = `
+      <img class="galeria__productos_items__foto" src="${imagen}"/>
+  
+      <div class="galeria__productos__textCard">
+  
+          <p class="galeria__productos_items__nombre">
+              ${nombre}
+          </p>
+          <p class="galeria__productos_items__precio destacado">
+              $${precio}
+          </p>
+          <a href="" class="galeria__productos--link destacado id="${id}">
+              Ver productos
+          </a>
+  
+      </div>  
+      `;
+    linea.innerHTML = content;
+    return linea;
+  
+    console.log(linea)
+  };
+  
+  const prodBox_saludBienestar = document.querySelector('[data-productosSaludBienestar]')
+  
+  productServices.productListSaludBienestar()
+    .then((data) => {
+      data.forEach( ( { imagen, nombre, precio, id } ) => {
+        // console.log(perfil)
+        const nuevaLinea = agregarProdSaludBienestar( imagen, nombre, precio, id );
+        prodBox_saludBienestar.appendChild(nuevaLinea);
+      });
+    }).catch((error) => alert("Ocurrio un errorcito"));
+
+
+
+
+//agregar prod camisetas
+const agregarProdCamisetas = (imagen, nombre, precio, id) => {
+    const linea = document.createElement("li");
+    linea.classList.add("galeria__productos_items");
+    const content = `
+      <img class="galeria__productos_items__foto" src="${imagen}"/>
+  
+      <div class="galeria__productos__textCard">
+  
+          <p class="galeria__productos_items__nombre">
+              ${nombre}
+          </p>
+          <p class="galeria__productos_items__precio destacado">
+              $${precio}
+          </p>
+          <a href="" class="galeria__productos--link destacado id="${id}">
+              Ver productos
+          </a>
+  
+      </div>  
+      `;
+    linea.innerHTML = content;
+    return linea;
+  
+    console.log(linea)
+  };
+  
+  const prodBox_camisetas = document.querySelector('[data-productosCamisetas]')
+  
+  productServices.productListCamisetas()
+    .then((data) => {
+      data.forEach( ( { imagen, nombre, precio, id } ) => {
+        // console.log(perfil)
+        const nuevaLinea = agregarProdCamisetas( imagen, nombre, precio, id );
+        prodBox_camisetas.appendChild(nuevaLinea);
+      });
+    }).catch((error) => alert("Ocurrio un errorcito"));
+
+
+
+
+
+//Agregar lineas prod diversos
+const agregarProdDiversos = (imagen, nombre, precio, id) => {
   const linea = document.createElement("li");
   linea.classList.add("galeria__productos_items");
   const content = `
@@ -46,13 +131,13 @@ const agregarProd = (imagen, nombre, precio, id) => {
   console.log(linea)
 };
 
-const prodBox = document.querySelector('[data-productos]')
+const prodBox_diversos = document.querySelector('[data-productosDiversos]')
 
-productServices.productList()
+productServices.productListDiversos()
   .then((data) => {
     data.forEach( ( { imagen, nombre, precio, id } ) => {
       // console.log(perfil)
-      const nuevaLinea = agregarProd( imagen, nombre, precio, id );
-      prodBox.appendChild(nuevaLinea);
+      const nuevaLinea = agregarProdDiversos( imagen, nombre, precio, id );
+      prodBox_diversos.appendChild(nuevaLinea);
     });
   }).catch((error) => alert("Ocurrio un errorcito"));
--- service/product-service.js
+++ service/product-service.js
@@ -1,9 +1,31 @@
-const productList = () => fetch("http://localhost:3000/diversos")
-  .then((response) => response.json()
-);
+const productListSaludBienestar = () => fetch("http://localhost:3000/saludBienestar").then((response) => response.json());
+
+const  productListCamisetas = () => fetch("http://localhost:3000/camisetas").then((response) => response.json());
+
+const productListDiversos = () => fetch("http://localhost:3000/diversos").then((response) => response.json());
 
 
-const addProduct = (imagen, nombre, precio) => {
+const addProductSaludBienestar = (imagen, nombre, precio) => {
+  return fetch("http://localhost:3000/SaludBienestar", {
+    method:"POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({imagen, nombre, precio, id: uuid.v4()}),
+  });
+};
+
+const addProductCamisetas = (imagen, nombre, precio) => {
+  return fetch("http://localhost:3000/camisetas", {
+    method:"POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({imagen, nombre, precio, id: uuid.v4()}),
+  });
+};
+
+const addProductDiversos = (imagen, nombre, precio) => {
   return fetch("http://localhost:3000/diversos", {
     method:"POST",
     headers: {
@@ -15,8 +37,12 @@ const addProduct = (imagen, nombre, precio) => {
 
 
 
-
 export const productServices = {
-  productList,
-  addProduct,
+  productListSaludBienestar,
+  productListCamisetas,
+  productListDiversos,
+  
+  addProductSaludBienestar,
+  addProductCamisetas,
+  addProductDiversos,
 };
